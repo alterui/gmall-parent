6ecab1a97387b86620f11e97449f72feb7e2d5ce
alterui <656812771@qq.com>
2020-03-15 23:43:18 +0800

    三级分类查询&加入redis缓存&学会使用RedisTemplate操作redis

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/MemberPriceMapper.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/MemberPriceMapper.java
@@ -2,6 +2,7 @@ package com.atguigu.gmall.pms.mapper;
 
 import com.atguigu.gmall.pms.entity.MemberPrice;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author Lfy
  * @since 2019-05-08
  */
+@Repository
 public interface MemberPriceMapper extends BaseMapper<MemberPrice> {
 
 }
